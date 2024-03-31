3cc9302f385cdd0c4cfae0e6fe2d8315923ecbf7
Jahid Hasan <jahidhasancmt7@gmail.com>
2024-03-31 12:06:58 +0600

    meeting details saved

--- src/view/meetings/meetings-create.vue
+++ src/view/meetings/meetings-create.vue
@@ -3,17 +3,50 @@ import { reactive, onBeforeMount, ref } from 'vue';
 import Icon from '@/components/icon/LucideIcon.vue'
 import { useRouter, useRoute, RouterView } from 'vue-router' 
 import axios from 'axios'  
+import { toast } from "vue3-toastify"; 
+
 const route = useRoute();
 const router = useRouter();
 
-const meetingData = ref([]);
+const meetingData = reactive({
+    id: 0,
+    user_id: 0,
+    title: '',
+    description: '',
+    meeting_type: '',
+    duration: '',
+    meeting_locations: [
+        {
+        location:'',
+        address:''
+        }
+    ],
+    meeting_category: '',
+});
+
+function addMoreLocations(){
+    meetingData.meeting_locations.push({
+    location:'',
+    address:'',
+  })
+}
+
 const meetingId = route.params.id;
 
  const fetchMeeting = async () => {
     try { 
         const response = await axios.get(tfhb_core_apps.admin_url + '/wp-json/hydra-booking/v1/meetings/'+meetingId);
         if (response.data.status == true) { 
-            meetingData.value = response.data.meeting
+            meetingData.id = response.data.meeting.id
+            meetingData.user_id = response.data.meeting.user_id
+            meetingData.title = response.data.meeting.title
+            meetingData.description = response.data.meeting.description
+            meetingData.meeting_type = response.data.meeting.meeting_type
+            meetingData.duration = response.data.meeting.duration
+            meetingData.meeting_category = response.data.meeting.meeting_category
+            if(response.data.meeting.meeting_locations){
+                meetingData.meeting_locations = JSON.parse(response.data.meeting.meeting_locations)
+            }
         }else{ 
             router.push({ name: 'MeetingsLists' });
         }
@@ -25,6 +58,19 @@ onBeforeMount(() => {
     fetchMeeting();
 });
 
+
+const UpdateMeetingData = async () => {
+    try { 
+        const response = await axios.post(tfhb_core_apps.admin_url + '/wp-json/hydra-booking/v1/meetings/details/update', meetingData);
+        if (response.data.status == true) { 
+            toast.success(response.data.message); 
+        }else{
+            toast.error(response.data.message); 
+        }
+    } catch (error) {
+        console.log(error);
+    } 
+}
 </script>
 
 <template>
@@ -42,18 +88,18 @@ onBeforeMount(() => {
             <ul>
                 <!-- to route example like meetings/create/13/details -->
                 
-                <li><router-link :to="'/meetings/create/details'" exact :class="{ 'active': $route.path === '/meetings/create/details' }"> Details</router-link></li> 
-                <li><router-link :to="'/meetings/create/availability'" :class="{ 'active': $route.path === '/meetings/create/availability' }"> Availability</router-link></li>  
-                <li><router-link :to="'/meetings/create/limits'" :class="{ 'active': $route.path === '/meetings/create/limits' }"> Limits</router-link></li>  
-                <li><router-link :to="'/meetings/create/questions'" :class="{ 'active': $route.path === '/meetings/create/questions' }"> Questions</router-link></li>  
-                <li><router-link :to="'/meetings/create/notifications'" :class="{ 'active': $route.path === '/meetings/create/notifications' }"> Notifications</router-link></li>  
-                <li><router-link :to="'/meetings/create/questions'" :class="{ 'active': $route.path === '/meetings/create/questions' }"> Payment</router-link></li>  
+                <li><router-link :to="'/meetings/single/'+ $route.params.id +'/details'" exact :class="{ 'active': $route.path === '/meetings/single/'+ $route.params.id +'/details' }"> Details</router-link></li> 
+                <li><router-link :to="'/meetings/single/'+ $route.params.id +'/availability'" :class="{ 'active': $route.path === '/meetings/single/'+ $route.params.id +'/availability' }"> Availability</router-link></li>  
+                <li><router-link :to="'/meetings/single/'+ $route.params.id +'/limits'" :class="{ 'active': $route.path === '/meetings/single/'+ $route.params.id +'/limits' }"> Limits</router-link></li>  
+                <li><router-link :to="'/meetings/single/'+ $route.params.id +'/questions'" :class="{ 'active': $route.path === '/meetings/single/'+ $route.params.id +'/questions' }"> Questions</router-link></li>  
+                <li><router-link :to="'/meetings/single/'+ $route.params.id +'/notifications'" :class="{ 'active': $route.path === '/meetings/single/'+ $route.params.id +'/notifications' }"> Notifications</router-link></li>  
+                <li><router-link :to="'/meetings/single/'+ $route.params.id +'/questions'" :class="{ 'active': $route.path === '/meetings/single/'+ $route.params.id +'/questions' }"> Payment</router-link></li>  
 
             </ul>  
         </nav>
 
         <div class="tfhb-hydra-dasboard-content"> 
-            <router-view :meetingId ="meetingId" :meeting="meetingData" />
+            <router-view :meetingId ="meetingId" :meeting="meetingData" @add-more-location="addMoreLocations" @update-meeting="UpdateMeetingData" />
         </div> 
     </div>
 </template>
--- src/view/meetings/meetings-details.vue
+++ src/view/meetings/meetings-details.vue
@@ -7,6 +7,7 @@ import HbTextarea from '@/components/form-fields/HbTextarea.vue'
 import HbSwitch from '@/components/form-fields/HbSwitch.vue';
 import Icon from '@/components/icon/LucideIcon.vue'
 
+const emit = defineEmits(["add-more-location", "update-meeting"]); 
 const props = defineProps({
     meetingId: {
         type: Number,
@@ -42,6 +43,7 @@ const props = defineProps({
         <div class="tfhb-admin-card-box tfhb-flexbox tfhb-gap-16 tfhb-m-0"> 
             <!-- Duration -->
             <HbSelect 
+                v-model="meeting.duration" 
                 required= "true" 
                 :label="$tfhb_trans['Duration']"  
                 :selected = "1"
@@ -55,28 +57,39 @@ const props = defineProps({
             />
         </div>
 
-        <div class="tfhb-admin-card-box tfhb-flexbox tfhb-gap-16 tfhb-m-0"> 
-            <!-- Location -->
-            <HbSelect 
-                required= "true" 
-                :label="$tfhb_trans['Location']"  
-                :selected = "1"
-                placeholder="Select Time Format"  
-                :option = "{'12_hours': '30 minutes', '24_hours': '10 minutes'}" 
-                :width= "50"
-            />
-            <!-- Address -->
-            <HbText  
-                required= "true"  
-                :label="$tfhb_trans['Address']"  
-                selected = "1"
-                :placeholder="$tfhb_trans['Type Address']" 
-                :width= "50"
-            /> 
+        <div class="tfhb-admin-card-box tfhb-no-flexbox tfhb-m-0"> 
+            <div class="tfhb-flexbox tfhb-gap-16 tfhb-mb-24" v-for="(slocation, index) in meeting.meeting_locations" :key="index">
+                <!-- Location -->
+                <HbSelect 
+                    v-model="slocation.location" 
+                    required= "true" 
+                    :label="$tfhb_trans['Location']"  
+                    :selected = "1"
+                    :placeholder="$tfhb_trans['Location']" 
+                    :option = "{'12_hours': '30 minutes', '24_hours': '10 minutes'}" 
+                    :width= "50"
+                />
+                <!-- Address -->
+                <HbText  
+                    v-model="slocation.address" 
+                    required= "true"  
+                    :label="$tfhb_trans['Address']"  
+                    selected = "1"
+                    :placeholder="$tfhb_trans['Type Address']" 
+                    :width= "50"
+                /> 
+            </div>
+            <div class="tfhb-add-new-question">
+                <div class="new-location tfhb-flexbox tfhb-gap-8 tfhb-justify-normal" @click="emit('add-more-location')">
+                    <Icon name="PlusCircle" :width="20"/>
+                    {{ $tfhb_trans['Add Another Location'] }}
+                </div>
+            </div>
         </div>
 
         <!-- Category -->
         <HbSelect 
+            v-model="meeting.meeting_category" 
             required= "true" 
             :label="$tfhb_trans['Select Category']"  
             :selected = "1"
@@ -84,7 +97,7 @@ const props = defineProps({
             :option = "{'12_hours': 'Design System', '24_hours': '10 minutes'}" 
         />
 
-        <button class="tfhb-btn boxed-btn tfhb-flexbox" @click="UpdateGeneralSettings">{{ $tfhb_trans['Save & Continue'] }} </button>
+        <button class="tfhb-btn boxed-btn tfhb-flexbox" @click="emit('update-meeting')">{{ $tfhb_trans['Save & Continue'] }} </button>
         <!--Bookings -->
     </div>
 </template>
