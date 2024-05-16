01519f12cda4d0da08b51d14df684bb0bd4ce063
mdrejon <sydurrahmant1@gmail.com>
2024-05-16 17:52:48 +0600

    update

--- app/Content/Template/meeting-form.php
+++ app/Content/Template/meeting-form.php
@@ -15,6 +15,10 @@ defined( 'ABSPATH' ) || exit;
  */
 
 
+ $questions = isset($args['questions']) ? $args['questions'] : array();
+
+
+
 ?> 
 <div class="tfhb-meeting-booking-form" style="display:none">
     <?php 
@@ -28,11 +32,54 @@ defined( 'ABSPATH' ) || exit;
         <h3>Details</h3>
     </div>
 
+    <?php 
+        //  echo '<pre>';
+        //  print_r($questions);
+        //  echo '</pre>';
+    ?>
     <div class="tfhb-forms tfhb-flexbox">
-        <div class="tfhb-single-form">
-            <label for="name">Name *</label>
-            <input type="text" id="name">
-        </div>
+        <?php  
+            if(is_array($questions) && !empty($questions)): 
+                  
+                foreach($questions as $key => $question): 
+                 
+                    if(empty($question['type'])){
+                        continue;
+                    }
+
+                    $required_star = $question['required'] == 1 ? '*' : '';
+                    $required = $question['required'] == 1 ? 'required' : '';
+                    
+                    echo '<div class="tfhb-single-form">
+                            <label for="'.$question['label'].'">'.$question['placeholder'].' '.$required_star.'</label>';
+                            if($question['type'] == 'select'){
+
+                                echo '<select id="'.$question['label'].'" '.$required.'>';
+                                    foreach($question['options'] as $option){
+                                        echo '<option value="'.$option['value'].'">'.$option['label'].'</option>';
+                                    }
+                                echo '</select>';
+
+                            }elseif($question['type'] == 'textarea'){
+
+                                echo '<textarea id="'.$question['label'].'" '.$required.'></textarea>';
+
+                            }elseif($question['type'] == 'checkbox'){
+
+                                echo '<label for="'.$question['label'].'">
+                                        <input id="'.$question['label'].'" type="'.$question['type'].'" '.$required.'>
+                                        <span class="checkmark"></span> '.$question['placeholder'].'
+                                    </label>';
+
+                            }else{
+                                
+                                echo '<input id="'.$question['label'].'"  type="'.$question['type'].'" '.$required.' placeholder="'.$question['placeholder'].'">';
+                            }
+                    echo '</div>';
+         
+                endforeach;
+            endif;
+        ?>
         <div class="tfhb-single-form">
             <label for="email">Email *</label>
             <input type="text" id="email">
