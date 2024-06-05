f0eaeb07fc320fc85d88ada5268d0fcb5506a27c
Jahid Hasan <jahidhasancmt7@gmail.com>
2024-06-05 14:38:14 +0600

    select icon change

--- app/Content/Template/meeting-info.php
+++ app/Content/Template/meeting-info.php
@@ -149,8 +149,6 @@ $booking_data = isset($args['booking_data']) ? $args['booking_data'] : array();
                 ?> 
             </select>
             <div class="tfhb-timezone-icon ">
-                <!-- <img src="<?php echo THB_URL.'assets/app/images/globe.svg'; ?>" alt="globe">   -->
-
                 <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <g clip-path="url(#clip0_1911_10296)">
                     <path d="M9.99935 18.3334C14.6017 18.3334 18.3327 14.6024 18.3327 10C18.3327 5.39765 14.6017 1.66669 9.99935 1.66669C5.39698 1.66669 1.66602 5.39765 1.66602 10C1.66602 14.6024 5.39698 18.3334 9.99935 18.3334Z" stroke="#765664" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
@@ -164,7 +162,6 @@ $booking_data = isset($args['booking_data']) ? $args['booking_data'] : array();
                 </defs>
                 </svg>
             </div>
-            <!-- <img src="<?php echo THB_URL.'assets/app/images/chevrons-up-down.svg'; ?>" alt="chevrons">  -->
         </div>
     </div>
 
