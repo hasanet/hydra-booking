dce17a735c9b9aa7b91b1bb42b5db6f37c79093f
Jahid Hasan <jahidhasancmt7@gmail.com>
2024-06-27 14:39:32 +0600

    subscriber added

--- includes/hooks/Integrations.php
+++ includes/hooks/Integrations.php
@@ -21,8 +21,12 @@ class Integrations{
             foreach($integrationsdata as $hook){
 
                 // integrations
-                if( !empty($hook['events']) && in_array("Booking Completed", $hook['events']) && !empty($hook['status']) ){
+                if( !empty($hook['webhook']) && 'Mailchimp'==$hook['webhook'] && !empty($hook['events']) && in_array("Booking Completed", $hook['events']) && !empty($hook['status']) ){
                     $this->tfhb_mailchimp_callback($booking, $hook, $MeetingData->host_id);
+                }
+
+				if( !empty($hook['webhook']) && 'FluentCRM'==$hook['webhook'] && !empty($hook['events']) && in_array("Booking Completed", $hook['events']) && !empty($hook['status']) ){
+                    $this->tfhb_fluentcrm_callback($booking, $hook, $MeetingData->host_id);
                 }
             }
         }
@@ -97,6 +101,80 @@ class Integrations{
 		$body_data = array();
     }
 
+	// FluentCRM Callback
+	function tfhb_fluentcrm_callback($booking, $hook, $host){
+
+		global $wpdb;
+		// Check if table exists
+		$subscriber_table_name = $wpdb->prefix . 'fc_subscribers';
+		$table_exists = $wpdb->get_var("SHOW TABLES LIKE '$subscriber_table_name'") == $subscriber_table_name;
+
+		$subscriber_pivot_table_name = $wpdb->prefix . 'fc_subscriber_pivot';
+		$pivot_table_exists = $wpdb->get_var("SHOW TABLES LIKE '$subscriber_pivot_table_name'") == $subscriber_pivot_table_name;
+
+		if ($table_exists) {
+			// Table exists, prepare and insert data
+			$first_name = ! empty( $booking->attendee_name ) ? $booking->attendee_name : '';
+			$last_name = ! empty( $booking->attendee_last_name ) ? $booking->attendee_last_name : '';
+			$email = ! empty( $booking->email ) ? $booking->email : '';
+
+			// Prepare the data
+			$data = array(
+				'first_name' => $first_name,
+				'last_name' => $last_name,
+				'email' => $email
+			);
+
+			// Format for the data (to ensure proper SQL data types)
+			$format = array(
+				'%s',  // first_name is a string
+				'%s',  // last_name is a string
+				'%s',  // email is a string
+			);
+			// Insert data
+			$inserted = $wpdb->insert($subscriber_table_name, $data, $format);
+			$subscripber_id = $wpdb->insert_id;
+
+			if($pivot_table_exists && !empty($hook['lists'])){
+				// Prepare the data
+				$list_data = array(
+					'subscriber_id' => $subscripber_id,
+					'object_id' => $hook['lists'],
+					'object_type' => 'FluentCrm\App\Models\Lists'
+				);
+
+				// Format for the data (to ensure proper SQL data types)
+				$list_format = array(
+					'%d',  // subscriber_id is a integer
+					'%d',  // object_id is a integer
+					'%s',  // email is a string
+				);
+				// Insert data
+				$listinserted = $wpdb->insert($subscriber_pivot_table_name, $list_data, $format);
+			}
+			if($pivot_table_exists && !empty($hook['tags'])){
+				// Prepare the data
+				$list_data = array(
+					'subscriber_id' => $subscripber_id,
+					'object_id' => $hook['tags'],
+					'object_type' => 'FluentCrm\App\Models\Tag'
+				);
+
+				// Format for the data (to ensure proper SQL data types)
+				$list_format = array(
+					'%d',  // subscriber_id is a integer
+					'%d',  // object_id is a integer
+					'%s',  // email is a string
+				);
+				// Insert data
+				$listinserted = $wpdb->insert($subscriber_pivot_table_name, $list_data, $format);
+			}
+		}
+
+		
+
+	}
+
     /* Mailchimp config set */
 	private function set_config( $api_key = '', $path = '' ) {
 
@@ -130,7 +208,7 @@ class Integrations{
 
     /* Add members to mailchimp */
 	public function add_members( $api_key, $booking, $hook ) {
-        // var_dump($booking); exit();
+   
 		$subscriber_email = ! empty( $booking->email ) ? $booking->email : '';
 
 		if ( $api_key != '' && $subscriber_email != '' ) {
